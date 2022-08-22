17ec7b845fd07630d78a629a68fae83014f7188a
luis.poveda93 <luis.poveda9321@gmail.com>
2022-08-22 22:17:42 +0200

    improving card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,7 @@
-import React, { useRef } from 'react';
+import React from 'react';
+import DropdownMenu from './DropdownMenu';
 
 const Card = ({image, ...props}) => {
-
-    const menu = useRef(null);
-    const dropMenu = () => {
-        menu.current.classList.toggle("hidden");
-    };
 
     let url = "";
     url = image.replace("ipfs//", "https://ipfs.io/ipfs/");
@@ -16,36 +12,15 @@ const Card = ({image, ...props}) => {
                 <a href="#!">
                     <img className="rounded-t-lg" src={url} alt=""/>
                 </a>
-                <div className="p-6">
+                <div className="relative p-6">
                     <h5 className="text-gray-900 text-xl font-medium mb-2">{props.name}</h5>
                     <p className="text-gray-700 text-base mb-4">
                         {props.description}
                     </p>
-                    <button type="button" onClick={()=>dropMenu()} className="inline-flex absolute right-0 bottom-0 items-center p-2 m-2 text-sm font-medium text-center text-gray-900 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none dark:text-white focus:ring-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
-                        <svg className="w-3 h-3" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z"></path></svg>
-                    </button>
-
-                    {/* Dropdown Menu */}
-
-                    <div id="dropdownDots" ref={menu} className="hidden z-10 w-44 absolute right-0 bottom-0 m-2 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600">
-                        <ul className="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownMenuIconButton">
-                            <li>
-                                <a href="#" className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
-                            </li>
-                            <li>
-                                <a href="#" className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
-                            </li>
-                            <li>
-                                <a href="#" className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
-                            </li>
-                        </ul>
-                        <div className="py-1">
-                            <a href="#" className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Separated link</a>
-                        </div>
-                    </div>
-                </div>
+                    <DropdownMenu /> 
             </div>
         </div>
+    </div>
     )
 }
 
--- src/pages/MyBooks.js
+++ src/pages/MyBooks.js
@@ -26,7 +26,7 @@ const MyBooks = () => {
     
 
     return (
-        <div className='grid grid-cols-3 gap-3 mt-1'>
+        <div className='grid grid-cols-1 gap-3 mt-1 lg:grid-cols-3'>
             {data.nfts.map((nft)=>{
                 return (
                     <div>
