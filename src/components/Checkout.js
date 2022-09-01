4201f5e3822afa1e33a16da8b0dc22d84b83c595
luis.poveda93 <luis.poveda9321@gmail.com>
2022-09-02 00:24:05 +0200

    implementing buy function

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,10 +1,28 @@
 /* This example requires Tailwind CSS v2.0+ */
-import { Fragment, useRef, useState } from 'react'
-import { Dialog, Transition } from '@headlessui/react'
+import { Fragment, useRef, useState } from 'react';
+import { Dialog, Transition } from '@headlessui/react';
+import { useMarketplace, useBuyNow } from '@thirdweb-dev/react';
+import { useAccount } from 'wagmi';
+import { ListingType } from '@thirdweb-dev/sdk';
 
 export default function Checkout({isOpen, setIsOpen}) {
 
-  const cancelButtonRef = useRef(null)
+  const cancelButtonRef = useRef(null);
+  const [pending, setPending] = useState(false);
+
+  const marketplace = useMarketplace(process.env.REACT_APP_MARKETPLACE_CONTRACT);
+  const { address } = useAccount();
+
+  const {
+    mutate: buyNow,
+    isLoading,
+    error,
+  } = useBuyNow(marketplace);
+
+  const buyingBook = () => {
+    console.log(buyNow({ listingId: 1, type: ListingType.Direct, buyAmount: 1, buyForWallet: address }));
+    isLoading? setPending(true) : setPending(false);
+  }
 
   return (
     <Transition.Root show={isOpen} as={Fragment}>
@@ -35,17 +53,17 @@ export default function Checkout({isOpen, setIsOpen}) {
               <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
                 <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                   <div className="sm:flex sm:items-start">
-                    <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                    <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-blue-100 sm:mx-0 sm:h-10 sm:w-10">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0H24V24H0z"/><path d="M12 19c.828 0 1.5.672 1.5 1.5S12.828 22 12 22s-1.5-.672-1.5-1.5.672-1.5 1.5-1.5zm0-17c3.314 0 6 2.686 6 6 0 2.165-.753 3.29-2.674 4.923C13.399 14.56 13 15.297 13 17h-2c0-2.474.787-3.695 3.031-5.601C15.548 10.11 16 9.434 16 8c0-2.21-1.79-4-4-4S8 5.79 8 8v1H6V8c0-3.314 2.686-6 6-6z"/></svg>
                       {/* <ExclamationTriangleIcon className="h-6 w-6 text-red-600" aria-hidden="true" /> */}
                     </div>
                     <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
                       <Dialog.Title as="h3" className="text-lg font-medium leading-6 text-gray-900">
-                        Deactivate account
+                        Complete checkout
                       </Dialog.Title>
                       <div className="mt-2">
                         <p className="text-sm text-gray-500">
-                          Are you sure you want to deactivate your account? All of your data will be permanently
-                          removed. This action cannot be undone.
+                          Do you want to buy this book?
                         </p>
                       </div>
                     </div>
@@ -54,10 +72,11 @@ export default function Checkout({isOpen, setIsOpen}) {
                 <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
                   <button
                     type="button"
-                    className="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
-                    onClick={() => setIsOpen(false)}
+                    disabled={pending}
+                    className="inline-flex w-full justify-center rounded-md border border-transparent bg-blue-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
+                    onClick={() => buyingBook()}
                   >
-                    Deactivate
+                    Buy
                   </button>
                   <button
                     type="button"
