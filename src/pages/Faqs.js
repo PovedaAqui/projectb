5bf48ca16e216fdaf746c17be8d7b8283cef50f1
luis.poveda93 <luis.poveda9321@gmail.com>
2022-09-11 01:35:45 +0200

    faqs page updated

--- src/pages/Faqs.js
+++ src/pages/Faqs.js
@@ -65,7 +65,7 @@ const Faqs = () => {
                                 <svg className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clipRule="evenodd"></path></svg>
                                 Is this an open-source project?
                             </h3>
-                            <p className="text-gray-500 dark:text-gray-400">Yes, you can find more information about license in our <a href="https://docs.polygon.technology/docs/develop/metamask/config-polygon-on-metamask/" className="font-medium underline text-primary-600 dark:text-primary-500 hover:no-underline" target="_blank" rel="noreferrer">Github repo</a>.</p>
+                            <p className="text-gray-500 dark:text-gray-400">Yes, you can find more information about license in our <a href="https://github.com/PovedaAqui/projectb" className="font-medium underline text-primary-600 dark:text-primary-500 hover:no-underline" target="_blank" rel="noreferrer">Github repo</a>.</p>
                         </div>
                     </div>
                 </div>
