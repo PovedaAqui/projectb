b1c4c0710ce2dc5a20dac1a9386fcb93cbdc8fd2
luis.poveda93 <luis.poveda9321@gmail.com>
2022-09-11 02:16:01 +0200

    navbar and public index updated

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -73,22 +73,24 @@ export default function NavBar() {
           </div>
 
           <Disclosure.Panel className="sm:hidden">
+          {({ close }) => (
             <div className="px-2 pt-2 pb-3 space-y-1">
               {navigation.map((item) => (
-                <Disclosure.Button
+                <NavLink
                   key={item.name}
                   as="a"
-                  href={item.href}
-                  className={classNames(
-                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                  to={item.href}
+                  className={({isActive}) => classNames(
+                    isActive ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
                     'block px-3 py-2 rounded-md text-base font-medium'
                   )}
                   aria-current={item.current ? 'page' : undefined}
+                  onClick={() => close()}
                 >
                   {item.name}
-                </Disclosure.Button>
+                </NavLink>
               ))}
-            </div>
+            </div>)}
           </Disclosure.Panel>
         </>
       )}
