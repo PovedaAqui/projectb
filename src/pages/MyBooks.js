011a7dfc961f006ead9a3ee4a8baf71cd1578c46
luis.poveda93 <luis.poveda9321@gmail.com>
2022-10-04 13:45:17 +0200

    working in the new MyBooks page

--- src/pages/MyBooks.js
+++ src/pages/MyBooks.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { useAccount } from 'wagmi';
-import { useQuery } from '@tanstack/react-query';
+import { useQueries, useQuery } from '@tanstack/react-query';
 import Card from '../components/Card';
+import { type } from '@testing-library/user-event/dist/type';
 
 const MyBooks = () => {
 
@@ -9,32 +10,59 @@ const MyBooks = () => {
     const chain = process.env.REACT_APP_CHAIN;
     const nftContract = process.env.REACT_APP_NFTCONTRACT;
 
-    const fetchNFT = async () => {
-        const res = await fetch(`https://api.nftport.xyz/v0/accounts/${address}?chain=${chain}&include=metadata&contract_address=${nftContract}`, {
+    const fetchBalance = async () => {
+        const res = await fetch(`https://api-eu1.tatum.io/v3/multitoken/address/balance/${chain}/${address}`, {
             "method": "GET",
             "headers": {
                 "Content-Type": "application/json",
-                "Authorization": process.env.REACT_APP_NFT_PORT
+                "Authorization": process.env.REACT_APP_TATUM
+                }
+            }
+        )
+        return res.json();
+    };
+
+    const { data: balance } = useQuery(['nfts'], fetchBalance);
+
+    const balances = balance?.find(element => element?.contractAddress === "0xf66f067a0a9a525e5a8b8e7a732857653e6994f5")?.balances
+    const tokenIds = balances?.map(element => element?.tokenId)
+   
+    const fetchMetadata = async ({ids}) => {
+        const res = await fetch(`https://api-eu1.tatum.io/v3/multitoken/metadata/${chain}/${nftContract}/${ids}`, {
+            "method": "GET",
+            "headers": {
+                "Content-Type": "application/json",
+                "Authorization": process.env.REACT_APP_TATUM
                 }
             }
         )
         return res.json();
     };
     
-    const { isLoading, data } = useQuery(['nfts'], fetchNFT, { enabled: isConnected });
+    const { data: metadata } = useQueries({
+        queries: tokenIds?.map(ids => {
+            return {
+                queryKey: ['ipfs', ids],
+                queryFn: () => fetchMetadata(ids),
+                enabled: tokenIds!==undefined && tokenIds!==null,
+            }
+        }),
+    })
 
-    return (
-        <div className='grid grid-cols-1 gap-y-3 gap-x-0 mt-1 lg:grid-cols-4'>
-            {isConnected && isLoading? <h1>Loading your books...</h1> : data?.nfts.map((nft, id)=>{
-                return (
-                    <div key={id}>
-                        <Card name={nft?.metadata?.name} description={nft?.metadata?.description} image={nft?.metadata?.image} external_url={nft?.metadata?.external_url} tokenId={nft?.token_id} />
-                    </div>
-                )
-            })}
-            {!isConnected && <div className='flex m-auto justify-center item-center leading-none text-lg font-extrabold text-gray-900 md:text-3xl md:ml-2 md:absolute md:mt-2 lg:text-4xl'>It's very quiet round here...</div>}
-        </div>
-    )
+    // console.log(metadata)
+
+    // return (
+    //     <div className='grid grid-cols-1 gap-y-3 gap-x-0 mt-1 lg:grid-cols-4'>
+    //         {isConnected && isLoading? <h1>Loading your books...</h1> : data?.nfts.map((nft, id)=>{
+    //             return (
+    //                 <div key={id}>
+    //                     <Card name={nft?.metadata?.name} description={nft?.metadata?.description} image={nft?.metadata?.image} external_url={nft?.metadata?.external_url} tokenId={nft?.token_id} />
+    //                 </div>
+    //             )
+    //         })}
+    //         {!isConnected && <div className='flex m-auto justify-center item-center leading-none text-lg font-extrabold text-gray-900 md:text-3xl md:ml-2 md:absolute md:mt-2 lg:text-4xl'>It's very quiet round here...</div>}
+    //     </div>
+    // )
 };
 
 export default MyBooks;
