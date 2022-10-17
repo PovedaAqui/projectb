5484ced6ac43bdc11abb8e3f4b9fb48d82039395
luis.poveda93 <luis.poveda9321@gmail.com>
2022-10-17 20:55:56 +0200

    working on mybooks

--- src/pages/MyBooks.js
+++ src/pages/MyBooks.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useAccount } from 'wagmi';
 import { useQueries, useQuery } from '@tanstack/react-query';
 import Card from '../components/Card';
+import { data } from 'autoprefixer';
 
 const MyBooks = () => {
 
     const { address, isConnected } = useAccount();
+    const [tokens, setTokens] = useState(null);
+    const [totalBalance, setTotalBalance] = useState(null);
+    const [ipfs, setIpfs] = useState(null);
     const chain = process.env.REACT_APP_CHAIN;
     const nftContract = process.env.REACT_APP_NFTCONTRACT;
 
     const fetchBalance = async () => {
-        const res = await fetch(`https://justcors.com/tl_3e02090/https://api-eu1.tatum.io/v3/multitoken/address/balance/${chain}/${address}`, {
+        const res = await fetch(`https://justcors.com/tl_21b1f06/https://api-eu1.tatum.io/v3/multitoken/address/balance/${chain}/${address}`, {
             "method": "GET",
             "headers": {
                 "Content-Type": "application/json",
@@ -18,17 +22,20 @@ const MyBooks = () => {
                 }
             }
         )
-        return res.json();
+        .then(res => res.json())
+        .then(res2 => res2.find(element => element?.contractAddress === "0xf66f067a0a9a525e5a8b8e7a732857653e6994f5"))
+        .then(res3 => res3.balances.map(element => element?.tokenId))
+        .then(res4 => setTotalBalance(res4))
     };
 
     //First call
     const { data: balance } = useQuery(['nfts'], fetchBalance);
 
-    const balances = balance && balance?.find(element => element?.contractAddress === "0xf66f067a0a9a525e5a8b8e7a732857653e6994f5");
-    const tokenIds = balances && balances?.balances?.map(element => element?.tokenId);
-   
-    const fetchMetadata = async (ids) => {
-        const res = await fetch(`https://justcors.com/tl_3e02090/https://api-eu1.tatum.io/v3/multitoken/metadata/${chain}/${nftContract}/${ids}`, {
+    useEffect(() => {
+        const fetchMetadata = async (ids) => {
+            let params = [];
+            let res = [];
+            res = await fetch(`https://justcors.com/tl_21b1f06/https://api-eu1.tatum.io/v3/multitoken/metadata/${chain}/${nftContract}/${ids}`, {
             "method": "GET",
             "headers": {
                 "Content-Type": "application/json",
@@ -36,21 +43,58 @@ const MyBooks = () => {
                 }
             }
         )
-        return res.json();
+        .then(res => res.json())
+        .then(data => params.push(data))
+        .then(data2 => data2 === params.length && setTokens(params))
     };
+        totalBalance!==null && totalBalance.map(ids => fetchMetadata(ids));
+    }, [!!totalBalance]);
+    
+    useEffect(() => {
+        const fetchIPFS = async (ids) => {
+            let url = "";
+            if (ids.data.includes("ipfs://")) {
+                url = ids.data.replace("ipfs://", "https://nftstorage.link/ipfs/");
+            }
+            else if (ids.data.includes("ipfs//")) {
+                url = ids.data.replace("ipfs//", "https://nftstorage.link/ipfs/");
+            } else {
+                url = ids;
+            }
+            let res = [];
+            res = await fetch(`https://justcors.com/tl_21b1f06/${url}`, {
+            "method": "GET",
+            "headers": {
+                "Content-Type": "application/json",
+                }
+            }
+        )
+        .then(res => res.json())
+        .then(data => console.log(data))
+        // .then(data2 => setIpfs(data2))
+    };
+        tokens!==null && tokens.map(data => fetchIPFS(data));
+    }, [!!tokens]);
+
+
+
+
+
     
     //Second call
-    const { data: metadata } = useQueries({
-        queries: (tokenIds || []).map(ids => {
-            return {
-                queryKey: ['ipfs', ids],
-                queryFn: () => fetchMetadata(ids),
-                enabled: !!tokenIds,
-            }
-        }),
-    })
+    // const { data: metadata } = useQueries({
+    //     queries: (tokenIds || []).map(ids => {
+    //         return {
+    //             queryKey: ['ipfs', ids],
+    //             queryFn: () => fetchMetadata(ids),
+    //             enabled: !!tokenIds,
+    //         }
+    //     }),
+    // })
 
-    const url = metadata && metadata?.replace("ipfs//", "https://nftstorage.link/ipfs/");
+    // const { data: metadata } = useQuery(['metadata', balance], fetchMetadata(balance), {enabled: balance!==null});
+
+    // const url = metadata && (metadata || []).replace("ipfs//", "https://nftstorage.link/ipfs/");
 
     // const fetchIPFS = async (urls) => {
     //     const res = await fetch(urls, {
