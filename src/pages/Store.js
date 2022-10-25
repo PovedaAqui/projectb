d9b9e4c73a31a667ad0b37f028b7a9b4c1b192df
luis.poveda93 <luis.poveda9321@gmail.com>
2022-10-25 20:38:53 +0200

    sdk updated

--- src/pages/Store.js
+++ src/pages/Store.js
@@ -1,31 +1,43 @@
 import React from 'react';
-import { useMarketplace, useActiveListings } from '@thirdweb-dev/react';
+import { useContract, useMarketplace, useActiveListings, useOwnedNFTs, useNFTBalance, useNFTs, useUnclaimedNFTSupply, useUnclaimedNFTs } from '@thirdweb-dev/react';
+import { useAccount } from 'wagmi';
 import Card2 from '../components/Card2';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import useDataFeed from '../utils/DataFeed';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { ThirdwebSDK } from "@thirdweb-dev/sdk";
 
 const queryClient = new QueryClient();
 
 const Store = () => {
+
+        const { address } = useAccount();
+        const { contract } = useContract(process.env.REACT_APP_DROP_CONTRACT);
+        const { data: nfts, isLoading, error } = useNFTs(contract, { start: 0, count: 100 });
+        return (
+            <div>{!isLoading && console.log(nfts)}</div>
+        )
+        
+        
     
-    const marketplace = useMarketplace(process.env.REACT_APP_MARKETPLACE_CONTRACT);
-    const { data: listings, isLoading, error } = useActiveListings(marketplace, { start: 0, count: 100 });
+//     const marketplace = useMarketplace(process.env.REACT_APP_MARKETPLACE_CONTRACT);
+//     const { data: listings, isLoading, error } = useActiveListings(marketplace, { start: 0, count: 100 });
 
-    const { data, isError, isFetching } = useDataFeed();
-    const tokenPrice = isFetching? "..." : (parseInt(data?.answer)*10**-8).toFixed(2);
+//     const { data, isError, isFetching } = useDataFeed();
+//     const tokenPrice = isFetching? "..." : (parseInt(data?.answer)*10**-8).toFixed(2);
 
-  return (
-    <div className='grid grid-cols-1 gap-3 mt-1 lg:grid-cols-3'>
-        {isLoading? <h1>Loading...</h1> : listings?.map((nfts, id) => {
-            return (
-                <div key={id}>
-                    <Card2 name={nfts.asset.name} description={nfts.asset.description} image={nfts.asset.image} price={nfts.buyoutCurrencyValuePerToken.displayValue} listingId={nfts.id} tokenPrice={tokenPrice}/>
-                </div>
-            )
-        })}
-    </div>
-  )
+//   return (
+//     <div className='grid grid-cols-1 gap-3 mt-1 lg:grid-cols-3'>
+//         {isLoading? <h1>Loading...</h1> : listings?.map((nfts, id) => {
+//             return (
+//                 <div key={id}>
+//                     <Card2 name={nfts.asset.name} description={nfts.asset.description} image={nfts.asset.image} price={nfts.buyoutCurrencyValuePerToken.displayValue} listingId={nfts.id} tokenPrice={tokenPrice}/>
+//                 </div>
+//             )
+//         })}
+//     </div>
+//   )
+
 }
 
 const hof = (WrappedComponent) => {
